d674fa9bc6df19ab6b1f05af7fcff2feacb2b7f6
Paul Wilson <paulanthonywilson@gmail.com>
2014-10-19 12:13:53 +0100

    tail recursing reversing

--- chap02/lib/reverse_list.ex
+++ chap02/lib/reverse_list.ex
@@ -7,4 +7,19 @@ defmodule ReverseList do
   def reverse(list) do
     list
   end
+
+
+  def tailrev(list) do
+    tailrev(list, [])
+  end
+
+
+  defp tailrev([], acc) do
+    acc
+  end
+  
+
+  defp tailrev([h|t], acc) do
+    tailrev(t, [h|acc])
+  end
 end
--- chap02/test/reverse_list_test.exs
+++ chap02/test/reverse_list_test.exs
@@ -1,6 +1,6 @@
 defmodule ReverseListTest do
   use ExUnit.Case
-  import ReverseList, only: [reverse: 1]
+  import ReverseList, only: [reverse: 1, tailrev: 1]
 
   test "no op reversing" do
     assert reverse([]) == []
@@ -12,5 +12,18 @@ defmodule ReverseListTest do
     assert reverse([:a, :b]) == [:b, :a]
     assert reverse([:a, :b, :c]) == [:c, :b, :a]
     assert reverse([:a, :b, :c, :d]) == [:d, :c, :b, :a]
+  end
+
+
+  test "tailrev no op reversing" do
+    assert tailrev([]) == []
+    assert tailrev([:a]) == [:a]
+  end
+
+
+  test "tailrev reversing" do
+    assert tailrev([:a, :b]) == [:b, :a]
+    assert tailrev([:a, :b, :c]) == [:c, :b, :a]
+    assert tailrev([:a, :b, :c, :d]) == [:d, :c, :b, :a]
   end
 end
