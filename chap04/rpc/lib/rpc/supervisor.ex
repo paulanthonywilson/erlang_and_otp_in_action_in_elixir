7a2e3b3208435cf53988310c184022ba3121cb4d
Paul Wilson <paulanthonywilson@gmail.com>
2014-10-23 15:33:55 +0100

    10 restarts in 6 seconds

--- chap04/rpc/lib/rpc/supervisor.ex
+++ chap04/rpc/lib/rpc/supervisor.ex
@@ -12,7 +12,7 @@ defmodule Rpc.Supervisor do
     IO.puts "innit"
     server = {:rpc_server, {Rpc.Server, :start_link, []}, :permanent, 2000, :worker, [Rpc.Server]}
     children = [server]
-    restart_strategy = {:one_for_one, 0, 1}
+    restart_strategy = {:one_for_one, 10, 60}
     {:ok, {restart_strategy, children}}
 
   end
