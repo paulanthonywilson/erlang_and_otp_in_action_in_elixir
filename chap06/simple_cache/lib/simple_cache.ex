b0ede3885f9b31b8d2d61e7b118a90ac87a2ce03
Paul Wilson <paulanthonywilson@gmail.com>
2014-10-27 22:34:57 +0000

    Chapter 6 done. Feeling I should add some kind of automated tests :-/

--- chap06/simple_cache/lib/simple_cache.ex
+++ chap06/simple_cache/lib/simple_cache.ex
@@ -1,6 +1,38 @@
 defmodule SimpleCache do
   use Application
 
+
+  ##
+  # API
+  def insert(key, value) do
+    case SimpleCache.Store.lookup(key) do
+      {:ok, pid} -> SimpleCache.Element.replace(pid, value)
+      {:error, _} ->
+        {:ok, pid} = SimpleCache.Element.create(value)
+        SimpleCache.Store.insert(key, pid)
+    end
+  end
+
+
+  def lookup(key) do
+    try do
+      {:ok, pid} = SimpleCache.Store.lookup(key)
+      {:ok, value} = SimpleCache.Element.fetch(pid)
+      {:ok, value}
+    rescue
+      _ ->
+      {:error, :not_found}
+    end
+  end
+
+
+  def delete(key) do
+    case SimpleCache.Store.lookup(key) do
+      {:ok, pid} -> SimpleCache.Element.delete pid
+      {:error, _reason} -> :ok
+    end
+  end
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
